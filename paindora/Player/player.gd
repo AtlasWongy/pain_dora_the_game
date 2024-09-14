e73698725f5546f8ae659b150fc51f7d54d7ae8e
AtlasWongy <atlasyjyj@gmail.com>
2024-09-14 17:00:59 +0800

    Refactor
    Going to switch back to move_and_slide instead

--- paindora/Player/player.gd
+++ paindora/Player/player.gd
@@ -9,6 +9,7 @@ enum ColorState {RED, BLUE, YELLOW}
 @onready var coyote_timer: Timer = $'CoyoteTimer'
 
 var gravity: float = 45.0
+var fall_gravity: float = 45.0
 var gravity_modifier: float = 2
 var jump_height: float = 48.0
 var apex_duration: float = 0.5
@@ -24,13 +25,13 @@ var last_floor = false
 var jump_timer: float = 0.0
 
 var color_map: Dictionary = {
-	"Z": Vector2i(2, 0),
-	"X": Vector2i(3, 0),
-	"C": Vector2i(1, 0)
+	"Left": Vector2i(2, 0),
+	"Down": Vector2i(3, 0),
+	"Right": Vector2i(1, 0)
 }
 
 func _ready() -> void:
-	color_state = color_map["Z"]
+	color_state = color_map["Left"]
 	coyote_timer.wait_time = coyote_frames / 60.0
 	coyote_timer.timeout.connect(_on_coyote_timer_timeout)
 	
@@ -55,10 +56,15 @@ func _physics_process(delta: float) -> void:
 	velocity = velocity.lerp(velocity, 5.0 * delta)
 	is_grounded = raycast.is_colliding()
 	is_ledge_grabbed = ledge_grabber.is_colliding()
-	move_and_collide(velocity * delta)
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	if collision != null:
+		if collision.get_collider() is TileMapLayer:
+			var tile_map_layer: TileMapLayer = collision.get_collider()
+			var collision_rid: RID = collision.get_collider_rid()
+			check_tile_color(tile_map_layer, collision_rid)
+			
 	if is_ledge_grabbed and !is_grounded:
 		grab_edge()
-		print("Grabbed Ledge")
 	if is_grounded:
 		is_jumping = false
 		jump_timer = 0.0
@@ -71,49 +77,22 @@ func _physics_process(delta: float) -> void:
 func grab_edge() -> void:
 	velocity.y -= jump_height
 
-# 	var collision = move_and_collide(velocity * delta)
-# 	if collision != null:
-# 		is_grounded = true
-# 		velocity.y = 0
-# 		jump_timer = 0.0
-# 		gravity /= 3
-# 	else:
-# 		is_grounded = false
-# 		if !is_grounded and last_floor and !is_jumping:
-# 			coyote = true
-# 			coyote_timer.start()
-# 	last_floor = is_grounded
-# 	jump_timer -= delta
-	
-	
-	
-# 		is_grounded = true
-# 		last_floor = true
-# 		var res_rid = collision.get_collider_rid()
-# 		var ray_collision: Object = raycast.get_collider()
-# 		if ray_collision is TileMapLayer and raycast.is_colliding():
-# 			is_grounded = true
-# 			check_tile_color(ray_collision, res_rid)
-# 	else:
-# 		is_grounded = false
-# 		if !is_grounded and last_floor and !is_jumping:
-# 			coyote = true
-# 			coyote_timer.start()
-			
 func check_tile_color(tile_map: TileMapLayer, rid: RID) -> void:
 	var tile_color: Vector2i = tile_map.get_cell_atlas_coords(tile_map.get_coords_for_body_rid(rid))
-	if color_state != tile_color:
-		print("Dont care first")
+	print(tile_color)
+# 	if get_color_state() != tile_color:
 # 		if timer.is_stopped():
 # 			timer.start()
 # 		await timer.timeout
-# 		if color_state != tile_color:
-# 			print("You die!")
+# 		if get_color_state() != tile_color:
 # 			get_tree().paused = true
 
 func change_color(event: InputEvent) -> void:
 	var color_select: String = OS.get_keycode_string(event.physical_keycode)
 	color_state = color_map[color_select]
+	
+func get_color_state() -> Vector2i:
+	return color_state
 	
 func _on_coyote_timer_timeout() -> void:
 	coyote = false
