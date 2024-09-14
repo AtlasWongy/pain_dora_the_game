b54f597b059a5cf106f6dc7eb58bea2e8aa3b36f
AtlasWongy <atlasyjyj@gmail.com>
2024-09-14 21:26:03 +0800

    Try Refactor

--- paindora/Platforms/Tile/tile.gd
+++ paindora/Platforms/Tile/tile.gd
@@ -1,4 +1,6 @@
 extends Resource
 class_name Tile
 
-
+@export var tile_pos: Vector2i
+@export var tile_color: Vector2i
+@export var is_skipped: bool
---
+++ paindora/Platforms/platform_procedural_generator.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name PlatformProceduralGenerator
+
+@export var platform: Platform
+@export var tile: Tile
